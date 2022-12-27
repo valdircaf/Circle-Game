7b6539c5c227361d1471ca8f2a95fdedb865ad00
Valdir Castro <valdircaf@hotmail.com>
2022-12-27 20:11:32 -0300

    jogoDosPontos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [dot, newDot] = useState([]);
+  const [undid, setUndid] = useState([]);
+
+  const handleDot = (e)=>{
+    const newSpan = {
+      clientX: e.clientX,
+      clientY: e.clientY
+    }
+
+    newDot((prev)=> [...prev, newSpan]);
+  }
+
+  const handleUndo = (e)=>{
+      e.stopPropagation();
+
+      const lastItem = dot[dot.length - 1];
+      setUndid((prev)=> [...prev, lastItem]);
+
+
+      newDot((prev)=>{
+        const newArr = [...prev].slice(0, -1);
+        return newArr;
+      })
+  }
+
+  const handleRedo = (e)=>{
+    e.stopPropagation();
+
+    if(undid.length === 0){
+      return;
+    }
+
+    const lastItem = undid[undid.length - 1];
+    newDot((prev)=> [...prev, lastItem]);
+    setUndid((prev)=>{
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    })
+  }
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" onClick={handleDot}>
+      <div className='btns'>
+      <button onClick={handleUndo}>Desfazer</button>
+      <button onClick={handleRedo}>Refazer</button>
+      </div>
+      {dot.map((item)=>(
+        <span className='span' style={{top: item.clientY, left: item.clientX}}/>
+      ))}
+     
+      
     </div>
   );
 }
 
 export default App;
+ 
+
+  
+
